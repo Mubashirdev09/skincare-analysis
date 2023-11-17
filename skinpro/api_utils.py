d5683b2c5a52923d36b52f4e2cfcaf5c33906ac7
Mubashir <rahmanksa2001@gmail.com>
2023-11-18 02:02:24 +0530

    done vercel settings

---
+++ skinpro/api_utils.py
@@ -0,0 +1,27 @@
+import time
+import requests
+
+
+API_SKINCARE_URL = "https://api-inference.huggingface.co/models/varun1505/face-characteristics"
+API_GENDER_URL = "https://api-inference.huggingface.co/models/Leilab/gender_class"
+headers = {"Authorization": "Bearer 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"}
+
+def query(file):
+    
+    with open(file, "rb") as f:
+        data = f.read()
+    skinres = requests.post(API_SKINCARE_URL, headers=headers, data=data)
+    genres = requests.post(API_GENDER_URL, headers=headers, data=data)
+    
+    # preprosessing data
+    gen_res_data = genres.json()
+    skin_res_data = [{k: v for k, v in reversed(item.items())} for item in skinres.json()]
+    gen_res = None
+    
+    if gen_res_data[0]["label"] == "men":
+        gen_res = "man"
+    else:
+        gen_res = "woman"
+        
+    time.sleep(3)
+    return {'skinres': skin_res_data,'genres': gen_res}
---
+++ skinpro/views.py
@@ -0,0 +1,31 @@
+from django.shortcuts import render, redirect
+from django.http import JsonResponse
+from .models import Imagemodel
+from .api_utils import query
+def index(request):
+    return render(request,"../templates/index.html")
+
+
+def upload_image(request):
+    if request.method == 'POST':
+        image = request.FILES['image']
+        new_image = Imagemodel(image=image)
+        new_image.save()
+        image_PATH = new_image.image.url[1:]
+
+        query_data = query(image_PATH)
+
+        request.session["query_data"] = query_data
+        request.session['image'] = image_PATH[7:]
+        # print("results",query_data)
+
+        return redirect('results')
+
+def results(request):
+    # Get the results from the query string or session (adjust as needed)
+    query_data = request.session.get("query_data", None)
+    image_path = request.session.get("image", None)
+    print(image_path)
+    return render(request, "results.html", {'results': query_data,'image_path':image_path})
+    
+
