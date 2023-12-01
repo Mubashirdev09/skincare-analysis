701b43319a92246fd152cb5895657b0193e822c8
Mubashir <rahmanksa2001@gmail.com>
2023-12-01 21:33:10 +0530

    version1

--- SkinAnalysis/settings.py
+++ SkinAnalysis/settings.py
@@ -121,4 +121,10 @@ USE_I18N = True
 
 USE_TZ = True
 
+STATIC_URL = '/static/'
+STATICFILES_DIRS=[
+    os.path.join(BASE_DIR, 'images'),
+]
+
+
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
